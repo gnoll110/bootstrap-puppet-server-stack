dbfa680cace82ade6e0046695bcb3e715aca88b1
Noel Kelly <gnoll110@yahoo.com.au>
2011-06-27 00:29:25 +1000

    3nd commit

--- config/deploy.rb
+++
@@ -1,9 +0,0 @@
-# Rename this file to deploy.rb
-
-# Fill slice_url in - where you're installing your stack to
-role :app, "musclepixs.com:9922"
-
-# Fill user in - if remote user is different to your local user
-# set :user, "username"
-
-default_run_options[:pty] = true
